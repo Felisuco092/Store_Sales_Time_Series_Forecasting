2919344ce9d6f4b0e3e962c3f771acf7dc403399
Felisuco092 <felisuco146@gmail.com>
2026-08-18 18:09:55 +0200

    feat: Month and Day features with all the pipeline of the preprocessing

--- src/features.py
+++ src/features.py
@@ -1,7 +1,10 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import FunctionTransformer, StandardScaler
+from sklearn.pipeline import Pipeline
 from config import TRAIN_CSV_PATH
 from data import load_data
+import numpy as np
 
 # The next class is a custom tranformer for the features days_since_start
 class DaysSinceStartTransformer(BaseEstimator, TransformerMixin):
@@ -29,39 +32,70 @@ class DaysSinceStartTransformer(BaseEstimator, TransformerMixin):
     transform(self, X, y=None)
         Calculates the number of days since the first date for each row in the input dataframe.
     """
-    def __init__(self, dayColumn='date'): 
+    def __init__(self, dayColumn='date', outputColumn='days_since_start'): 
         self.dayColumn = dayColumn
+        self.outputColumn = outputColumn
     def fit(self, X, y=None):
         self.diaPrimero_ = X[self.dayColumn].min()
         return self
     def transform(self, X, y=None):
         output = (X[self.dayColumn] - self.diaPrimero_).dt.days + 1
 
-        return output.to_frame()
+        return output.to_frame(self.outputColumn)
+
+######################
+
+def extract_month_year(df):
+    year = df['date'].dt.year
+    month = df['date'].dt.month
+
+    return np.column_stack((year, month))
+
+def features_dates():
+    return ['year', 'month']
+day_month_transformer = FunctionTransformer(extract_month_year, feature_names_out=features_dates)
 
 ######################
 
     
-def get_column_transformer():
+def get_date_transformer():
     """
     
-    Prepare a column transformer for the data to preprocces it before training the model
+    Prepare a pipeline transformer for the data to preprocces the date before training the model
 
     Returns:
-        ColumnTransformer: The column transformer
+        Pipeline: The pipeline transformer
     """
-    ct = ColumnTransformer([
+    ct_create = ColumnTransformer([
         ('days_since_start', DaysSinceStartTransformer(), ['date']),
-        ()
+        ('dates', day_month_transformer, ['date'])
+    ])
+    general_pipeline = Pipeline([
+        ('transform', ct_create),
+        ('scaler', StandardScaler())
+    ])
+    return general_pipeline
+
+def get_full_preprocessing():
+    """
+    Prepare the full pipeline with all the preprocessing steps
+
+    Returns: 
+        Pipeline: The pipeline with all the preprocessing steps
+    """
+
+    ct = ColumnTransformer([
+        ('dates', get_date_transformer(), ['date']),
+        ('remainder', 'passthrough', ['store_nbr', 'family'])
     ])
     return ct
 
-
+######################
 
 if __name__ == "__main__":
     train_df = load_data(TRAIN_CSV_PATH)
 
-    ct = get_column_transformer()
+    ct = get_date_transformer()
     trf = ct.fit(train_df)
 
     print(trf.transform(train_df))
